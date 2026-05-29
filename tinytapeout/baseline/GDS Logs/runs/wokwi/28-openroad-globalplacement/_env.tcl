2a32e6ec16d4e9b414b07719b270de9ceecd47a8
justin <chinwchun@gmail.com>
2026-05-29 17:57:26 +0800

    New GDS Changes

---
+++ tinytapeout/baseline/GDS Logs/runs/wokwi/28-openroad-globalplacement/_env.tcl
@@ -0,0 +1,74 @@
+set ::env(STEP_ID) OpenROAD.GlobalPlacement
+set ::env(TECH_LEF) /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd__nom.tlef
+set ::env(MACRO_LEFS) ""
+set ::env(STD_CELL_LIBRARY) sky130_fd_sc_hd
+set ::env(VDD_PIN) VPWR
+set ::env(GND_PIN) VGND
+set ::env(TECH_LEFS) "\"nom_*\" /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd__nom.tlef \"min_*\" /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd__min.tlef \"max_*\" /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd__max.tlef"
+set ::env(PRIMARY_GDSII_STREAMOUT_TOOL) magic
+set ::env(DEFAULT_CORNER) nom_tt_025C_1v80
+set ::env(STA_CORNERS) "nom_tt_025C_1v80 nom_ss_100C_1v60 nom_ff_n40C_1v95 min_tt_025C_1v80 min_ss_100C_1v60 min_ff_n40C_1v95 max_tt_025C_1v80 max_ss_100C_1v60 max_ff_n40C_1v95"
+set ::env(RT_MIN_LAYER) met1
+set ::env(RT_MAX_LAYER) met4
+set ::env(SCL_GROUND_PINS) "VGND VNB"
+set ::env(SCL_POWER_PINS) "VPWR VPB"
+set ::env(TRISTATE_CELLS) "\"sky130_fd_sc_hd__ebuf*\""
+set ::env(FILL_CELLS) "sky130_fd_sc_hd__fill_2 sky130_fd_sc_hd__fill_1"
+set ::env(DECAP_CELLS) sky130_fd_sc_hd__decap_3
+set ::env(LIB) "\"*_tt_025C_1v80\" /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib \"*_ss_100C_1v60\" /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__ss_100C_1v60.lib \"*_ff_n40C_1v95\" /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__ff_n40C_1v95.lib"
+set ::env(CELL_LEFS) "/home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/lef/sky130_ef_sc_hd.lef /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/lef/sky130_fd_sc_hd.lef"
+set ::env(CELL_GDS) /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/gds/sky130_fd_sc_hd.gds
+set ::env(CELL_VERILOG_MODELS) "/home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/verilog/primitives.v /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd.v"
+set ::env(CELL_BB_VERILOG_MODELS) "/home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd__blackbox.v /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/verilog/sky130_fd_sc_hd__blackbox_pp.v"
+set ::env(CELL_SPICE_MODELS) "/home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__decap_12.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__decap_20_12.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__decap_40_12.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__decap_60_12.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__decap_80_12.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__fill_12.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__fill_2.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__fill_4.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_ef_sc_hd__fill_8.spice /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice"
+set ::env(CELL_CDLS) /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.ref/sky130_fd_sc_hd/cdl/sky130_fd_sc_hd.cdl
+set ::env(SYNTH_EXCLUDED_CELL_FILE) /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.tech/openlane/sky130_fd_sc_hd/no_synth.cells
+set ::env(PNR_EXCLUDED_CELL_FILE) /home/runner/.ciel/ciel/sky130/versions/8afc8346a57fe1ab7934ba5a6056ea8b43078e71/sky130A/libs.tech/openlane/sky130_fd_sc_hd/drc_exclude.cells
+set ::env(OUTPUT_CAP_LOAD) 33.442
+set ::env(MAX_FANOUT_CONSTRAINT) 10
+set ::env(MAX_TRANSITION_CONSTRAINT) 0.75
+set ::env(MAX_CAPACITANCE_CONSTRAINT) 0.2
+set ::env(CLOCK_UNCERTAINTY_CONSTRAINT) 0.25
+set ::env(CLOCK_TRANSITION_CONSTRAINT) 0.1499999999999999944488848768742172978818416595458984375
+set ::env(TIME_DERATING_CONSTRAINT) 5
+set ::env(IO_DELAY_CONSTRAINT) 20
+set ::env(SYNTH_DRIVING_CELL) sky130_fd_sc_hd__inv_2/Y
+set ::env(SYNTH_TIEHI_CELL) sky130_fd_sc_hd__conb_1/HI
+set ::env(SYNTH_TIELO_CELL) sky130_fd_sc_hd__conb_1/LO
+set ::env(SYNTH_BUFFER_CELL) sky130_fd_sc_hd__buf_2/A/X
+set ::env(PLACE_SITE) unithd
+set ::env(CELL_PAD_EXCLUDE) "\"sky130_fd_sc_hd__tap*\" \"sky130_fd_sc_hd__decap*\" \"sky130_ef_sc_hd__decap*\" \"sky130_fd_sc_hd__fill*\""
+set ::env(DIODE_CELL) sky130_fd_sc_hd__diode_2/DIODE
+set ::env(WELLTAP_CELL) sky130_fd_sc_hd__tapvpwrvgnd_1
+set ::env(ENDCAP_CELL) sky130_fd_sc_hd__decap_3
+set ::env(DESIGN_NAME) tt_um_aes128_baseline
+set ::env(CLOCK_PERIOD) 100
+set ::env(CLOCK_PORT) clk
+set ::env(DIE_AREA) "0 0 1378.16 225.76"
+set ::env(FALLBACK_SDC) /nix/store/5qhi22zib758klv5di7jvny5b48ckmw3-python3-3.13.9-env/lib/python3.13/site-packages/librelane/scripts/base.sdc
+set ::env(PAD_EDGE_SPACING) 0
+set ::env(SET_RC_VERBOSE) 0
+set ::env(PDN_CONNECT_MACROS_TO_GRID) 1
+set ::env(PDN_ENABLE_GLOBAL_CONNECTIONS) 1
+set ::env(DEDUPLICATE_CORNERS) 0
+set ::env(GRT_ADJUSTMENT) 0.299999999999999988897769753748434595763683319091796875
+set ::env(GRT_MACRO_EXTENSION) 0
+set ::env(GRT_LAYER_ADJUSTMENTS) "0.99 0 0 0 0 0"
+set ::env(PL_OPTIMIZE_MIRRORING) 1
+set ::env(PL_MAX_DISPLACEMENT_X) 500
+set ::env(PL_MAX_DISPLACEMENT_Y) 100
+set ::env(DPL_CELL_PADDING) 0
+set ::env(RSZ_DONT_TOUCH_RX) "\$^"
+set ::env(PL_TARGET_DENSITY_PCT) 60
+set ::env(PL_SKIP_INITIAL_PLACEMENT) 0
+set ::env(PL_WIRE_LENGTH_COEF) 0.25
+set ::env(FP_CORE_UTIL) 50
+set ::env(GPL_CELL_PADDING) 0
+set ::env(PL_TIMING_DRIVEN) 0
+set ::env(PL_ROUTABILITY_DRIVEN) 1
+set ::env(CURRENT_ODB) /home/runner/work/ttsky-aes128-baseline/ttsky-aes128-baseline/runs/wokwi/27-odb-applydeftemplate/tt_um_aes128_baseline.odb
+set ::env(SAVE_ODB) /home/runner/work/ttsky-aes128-baseline/ttsky-aes128-baseline/runs/wokwi/28-openroad-globalplacement/tt_um_aes128_baseline.odb
+set ::env(SAVE_DEF) /home/runner/work/ttsky-aes128-baseline/ttsky-aes128-baseline/runs/wokwi/28-openroad-globalplacement/tt_um_aes128_baseline.def
+set ::env(SAVE_SDC) /home/runner/work/ttsky-aes128-baseline/ttsky-aes128-baseline/runs/wokwi/28-openroad-globalplacement/tt_um_aes128_baseline.sdc
+set ::env(SAVE_NL) /home/runner/work/ttsky-aes128-baseline/ttsky-aes128-baseline/runs/wokwi/28-openroad-globalplacement/tt_um_aes128_baseline.nl.v
+set ::env(SAVE_PNL) /home/runner/work/ttsky-aes128-baseline/ttsky-aes128-baseline/runs/wokwi/28-openroad-globalplacement/tt_um_aes128_baseline.pnl.v
